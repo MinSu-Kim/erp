020a4378cce2f5725f29b618de72d5a141579c02
teacher <net94.teacher@gmail.com>
2020-02-20 23:49:20 +0900

    model, tableView 추상화 완료

--- erp_application.py
+++ erp_application.py
@@ -1,10 +1,12 @@
 from PyQt5.QtWidgets import QApplication
 
-from table_view.custom_table_view import CustomTableViewWidget
+from table_view.abstract_table_view import AbstractTableViewWidget
 from table_view.department_table_view import DepartmentTableViewWidget
+from table_view.title_table_view import TitleTableViewWidget
 
 if __name__ == '__main__':
     app = QApplication([])
-    w = CustomTableViewWidget()
+    # w = CustomTableViewWidget()
     d = DepartmentTableViewWidget()
+    t = TitleTableViewWidget()
     app.exec()
--- table_view/abstract_table_view.py
+++ table_view/abstract_table_view.py
@@ -1,38 +1,36 @@
+from abc import ABCMeta, abstractmethod
+
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QAbstractItemView
 
-from model.custom_table_model import CustomTableModel
+from model.abstract_table_model import AbstractTableModel
 
 
-class CustomTableViewWidget(QWidget):
+class AbstractTableViewWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.ui = uic.loadUi('ui/table_view.ui')
 
         # create the view
         self.tableView = self.ui.custom_table_view
+        self.model = None
 
         # table view 설정
         self.set_table_view_config()
 
-        tble_data = [(1,'아메리카노'),(2, '카페모카')]
-        header = ['제품 코드', '제품 명']
-
-        # table에 data 로드
-        self.model = None
-        self.set_table_row_data(tble_data, header)
-
-        self.ui.show()
-
-    def set_table_row_data(self, tble_data, header):
-        self.model = CustomTableModel(data=tble_data, header=header)
-        self.tableView.setModel(self.model)
+    @abstractmethod
+    def set_column_size(self):
+        """
+        self.tableView.horizontalHeader().resizeSection(0, 40)
+        self.tableView.horizontalHeader().resizeSection(1, 50)
+        """
+        raise NotImplementedError("Subclass must implement abstract method")
 
     def set_table_view_config(self):
         # header size
-        self.tableView.horizontalHeader().resizeSection(0, 40)
-        self.tableView.horizontalHeader().resizeSection(1, 60)
+        self.set_column_size()
+
         self.tableView.horizontalHeader().setStyleSheet('QHeaderView::section{background:#66666666}')  # 배경색을 녹색
         # Set the alignment to the headers
         self.tableView.horizontalHeader().setDefaultAlignment(Qt.AlignCenter)
@@ -42,7 +40,7 @@ class CustomTableViewWidget(QWidget):
         self.tableView.setShowGrid(True)
         # row단위 선택
         self.tableView.setSelectionBehavior(QAbstractItemView.SelectRows)
-        # 헤더의 내용을 tableView의 크기에 맞춤
+        # 헤더의 내용을 tableView의 크기에 맞춤(아래 3가지 방법)
         self.tableView.horizontalHeader().setStretchLastSection(True)
         # 모든 컬럼의 사이즈를 동일하게 맞춤
         # self.tableView.resizeColumnsToContents()
